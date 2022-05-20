c84c0dcb3315772628d82663aa27a81cc931aaa2
xFIREx2005 <kaique.santos150@etec.sp.gov.br>
2022-05-19 22:02:39 -0300

    19/05
    fim fase 1

---
+++ Assets/Codes/Scena.cs
@@ -0,0 +1,41 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scena : MonoBehaviour
+{
+
+    public string cenaName;
+
+
+    public void Play()
+    {
+        cenaName = "Fase1";
+        SceneManager.LoadScene(cenaName);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (collision.CompareTag("Player"))
+            {
+                PlayerController player = collision.GetComponent<PlayerController>();
+                if (player != null)
+                {
+                    SceneManager.LoadScene(cenaName);
+                }
+            }
+
+        }
+    }
+
+
+}
